5077ca2e6e97a2cbfe075e56596a1d6ddd8a3549
Julio César <julio.gutierrez@bitnomial.com>
2022-11-04 17:05:28 -0600

    Replace turtle from aws-ec2-knownhosts executable
    Now we use optparse-applicative for parsing the CLI arguments.

--- exec/aws-ec2-knownhosts/Main.hs
+++ exec/aws-ec2-knownhosts/Main.hs
@@ -5,29 +5,34 @@ module Main where
 import AWS.KnownHosts (updateKnownHosts)
 import AWS.Types (Ec2Instance (..))
 import Data.Aeson (Result (..), fromJSON, json)
-import Filesystem.Path.CurrentOS (encodeString)
 import qualified System.IO.Streams as Streams
 import System.IO.Streams.Attoparsec (parseFromStream)
-import Turtle (FilePath, Parser, argPath, options)
-import Prelude hiding (FilePath)
+import Options.Applicative (Parser)
+import qualified Options.Applicative as Opt
 
 
 main :: IO ()
-main =
-    options "Update known_hosts file with pubkeys from EC2" args
-        >>= readKeys
-        >>= maybe noKeys updateKnownHosts
+main = do
+  Opt.execParser opts
+    >>= readKeys
+    >>= maybe noKeys updateKnownHosts
+  where
+    opts = Opt.info (Opt.helper <*> args)
+        $ Opt.fullDesc
+            <> Opt.progDesc "Update known_hosts file with pubkeys from EC2"
+            <> Opt.header "aws-ec2-knownhosts - Get EC2 pubkeys on your known_hosts file"
 
 
 args :: Parser FilePath
-args = argPath "pubkey_file" "Path to JSON pubkey file to update knownhosts"
+args =
+  Opt.strArgument $
+    Opt.metavar "PUBKEY_FILE" <> Opt.help "Path to JSON pubkey file to update knownhosts"
 
 
 readKeys :: FilePath -> IO (Maybe [Ec2Instance])
 readKeys =
     fmap (maybeResult . fromJSON)
         . flip Streams.withFileAsInput (parseFromStream json)
-        . encodeString
 
 
 maybeResult :: Result a -> Maybe a
